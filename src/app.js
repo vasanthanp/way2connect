1c09238482e006274e9e94de3227e40a50c17a86
vasanthanp <vasanthan322001@gmail.com>
2021-03-24 14:03:10 +0530

    Add mysql connection

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("backend running !!!!!!")
+//IMPORTING REQUIRED PACKAGES
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const {DBCONNECTION}  = require('./config/DB_Config')
+const PORT = process.env.PORT || 5000;
+//CONFIGURING EXPRESS APP 
+app.use(bodyParser.json());
+app.use(express.json());
+//CONNECTING DATABASE
+DBCONNECTION.connect(err => {
+    if(err) return console.log(`database connection error`,err.stack);
+    console.log(`database connected successfully as ${DBCONNECTION.threadId}`)
+})
+//CONFIGURING SERVER PORT
+app.listen(PORT,()=>console.log(`server listen at port ${PORT}`))
