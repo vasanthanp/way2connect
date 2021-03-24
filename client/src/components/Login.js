7004e0a259e863f52e6646d28b2d6f41ea2cafd9
Gajendhiranm <deepakjs1234@gmail.com>
2021-03-24 09:30:19 +0530

    24-March

---
+++ client/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import loginImage from "../images/loginImage.png";
+export default function Login() {
+    return (
+        <div>
+         <img src={loginImage} />
+        </div>
+    )
+}
