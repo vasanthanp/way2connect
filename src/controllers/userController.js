73b6732ee63589046b5a24c2ddaf1f0b5c583ffa
Gajendhiran M <48618592+Gajendhiranm@users.noreply.github.com>
2021-04-08 16:30:57 +0530

    Delete src directory

--- src/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-const {DBCONNECTION}  = require('../config/DB_Config')
-const signupController = (req,res) => {
-    const {username,email,password}  = req.body;
-}
-const signinController = (req,res) => {
-    const {email,password}  = req.body;
-
-}
-module.exports = {signinController,signupController}
--- src/routes/UserRoute.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-
-router.post('/signup',signupController);
-router.post('/signin',signinController);
